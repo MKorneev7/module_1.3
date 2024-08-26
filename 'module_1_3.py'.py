abcebdffdc77f18b31af547ffc35c6a8edf57d51
MKorneev7 <mik61349304@yandex.ru>
2024-08-27 00:50:59 +0300

    Add files via upload

---
+++ 'module_1_3.py'.py
@@ -0,0 +1,13 @@
+name='Mikhail'
+print(name, type(name))
+age=31
+print(age, type(age))
+a=15
+print(age+a, type(age+a))
+new_age=(age+a)
+is_student=True
+print(is_student, type(is_student))
+
+
+
+
